79543de539c9e2de26494d9c7f1c40399ecb2ed6
robileksono <robileksono@gmail.com>
2023-04-04 15:22:07 +0800

    auth
    add all files for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::group(['middleware' => ['role:admin']], function () {
+        //Dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    });
+});
+
+Route::get('/', [HomeController::class, 'index'])->name('landing');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
